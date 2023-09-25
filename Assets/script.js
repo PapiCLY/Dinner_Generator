7d26b3cd773c3105af6c84d76dd95bd22b045df1
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2023-09-25 01:06:04 -0400

    setup random food generator

--- Assets/script.js
+++ Assets/script.js
@@ -8,4 +8,6 @@ const dinnerBtn = document.querySelector('#dinnerBtn').addEventListener('click',
 
 showNewDinner = ()=>{
     const showDinner = document.querySelector('#dinnerTime')
+    const randomItem = food[Math.floor(Math.random()*food.length)]
+    console.log(randomItem)
 }
