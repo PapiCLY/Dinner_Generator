33825756dc610cfc74241115a84ba93e0f4e76eb
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2023-09-25 00:57:45 -0400

    initial JS setup

---
+++ Assets/script.js
@@ -0,0 +1,11 @@
+//globals
+let food = ['Lasagna', 'Curry Chicken & Rice', 'Baked Ziti', 'Speghetti and ground Turkey']
+const showDinner = document.querySelector('#dinnerTime')
+const newDinner = document.querySelector('#addToList').value
+const dinnerBtn = document.querySelector('#dinnerBtn').addEventListener('click', ()=>{
+    showNewDinner();
+})
+
+showNewDinner = ()=>{
+    const showDinner = document.querySelector('#dinnerTime')
+}
