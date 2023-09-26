87f3f474f5d1d0eb1c8157b81e1c391c2444f27d
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2023-09-25 21:45:18 -0400

    minor JS update

--- Assets/script.js
+++ Assets/script.js
@@ -28,7 +28,7 @@ newDinnerToEat = ()=>{
     } else{
         food.push(newDinner);
         clearField();
-        showDinner.innerHTML = `<span style="font-weight: bold; text-decoration: underline;">${newDinner}</span> has been added to the list!`;
+        showDinner.innerHTML = `<span style="font-weight: bold;">${newDinner}</span> has been added to the list!`;
         showDinner.style.color = 'green'
     }
 }
