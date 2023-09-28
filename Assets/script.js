157b2335d174a2a3c697d6cda08ea6d7632824f6
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2023-09-28 19:04:21 -0400

    added code for hamburger menu

--- Assets/script.js
+++ Assets/script.js
@@ -9,6 +9,13 @@ const newDinnerBtn = document.querySelector('#newDinnerBtn').addEventListener('c
     newDinnerToEat()
 })
 const numsAndChars = /[^a-zA-Z0-9\s]/;
+const hamburgerButton = document.querySelector('.hamburgerButton')
+const navLinks = document.querySelector('.contactBar ul')
+
+//Hamburger Menu
+hamburgerButton.addEventListener('click', ()=>{
+    navLinks.classList.toggle('show')
+})
 
 //function to show the random dinners
 showNewDinner = ()=>{
